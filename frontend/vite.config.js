fcfc1d6cba7653ca31d480ea5e7d1c0b736becfb
shazamakash1 <manirujjamanandlion@gmail.com>
2025-06-24 17:42:12 +0530

    prodfix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-const backendUrl = `${import.meta.env.VITE_BACKEND_URL}:${import.meta.env.VITE_PORT}` || 'http://localhost:3000';
+const backendUrl
+try {
+  backendUrl = `${import.meta.env.VITE_BACKEND_URL}:${import.meta.env.VITE_PORT}`;
+} catch (error) {
+  backendUrl = 'https://fullstack-base-plate.onrender.com';
+}
+  
 
 // https://vite.dev/config/
 export default defineConfig({
