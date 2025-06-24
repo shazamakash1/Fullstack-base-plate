17423500a784457ec86742331b45b0464e60187b
shazamakash1 <manirujjamanandlion@gmail.com>
2025-06-24 17:33:20 +0530

    minor change to automate url and port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+const backendUrl = `${import.meta.env.VITE_BACKEND_URL}:${import.meta.env.VITE_PORT}` || 'http://localhost:3000';
+
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy:{
-      '/api': 'http://localhost:3000', // Adjust the port if your backend server runs on a different port
+      '/api': backendUrl, // Adjust the port if your backend server runs on a different port
     },
   },
   plugins: [react()],
