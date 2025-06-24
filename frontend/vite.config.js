a565c98119870f95af71a1ddfa71078b720f516c
shazamakash1 <manirujjamanandlion@gmail.com>
2025-06-24 17:43:41 +0530

    prodfix-2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-const backendUrl
+let backendUrl='';
 try {
   backendUrl = `${import.meta.env.VITE_BACKEND_URL}:${import.meta.env.VITE_PORT}`;
 } catch (error) {
